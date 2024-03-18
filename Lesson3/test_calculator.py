f5c97db1243cf1fabfebe968beb84e2dd04c3db5
Dmytro Shelikhov <dimaschelichov228@gmail.com>
2024-03-18 19:46:37 +0100

    Add a lot of new files

---
+++ Lesson3/test_calculator.py
@@ -0,0 +1,11 @@
+import unittest
+from py3 import sum_two_variables
+
+class Testcalculator(unittest.TestCase):
+
+    def test_sum_two_variables_result_11(self):
+        sum = sum_two_variables(5,6)
+        self.assertEqual(sum,11)
+
+if __name__ == "__main__":
+    unittest.main()
